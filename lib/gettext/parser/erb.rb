8e295fc94e9ea1ec5b62be03d558029ab5e98c8c
Peter Schrammel <peter.schrammel@experteer.com>
2009-09-23 12:09:53 +0200

    added .plain .html

--- lib/gettext/parser/erb.rb
+++ lib/gettext/parser/erb.rb
@@ -17,7 +17,7 @@ module GetText
     extend self
 
     @config = {
-      :extnames => ['.rhtml', '.erb']
+      :extnames => ['.rhtml', '.erb','.html','.plain']
     }
 
     # Sets some preferences to parse ERB files.
